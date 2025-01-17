0c65aca95e2166695d2ef0947d72d8ba4e02e79a
Bishal Gautam <bishalkrgautam@gmail.com>
2025-01-17 16:36:44 -0600

    add courselist
    hook up to api, inputSignal

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,23 +1,47 @@
-import {Component, computed, effect, inject, Injector, signal} from '@angular/core';
-import {CoursesService} from "../services/courses.service";
-import {Course, sortCoursesBySeqNo} from "../models/course.model";
-import {MatTab, MatTabGroup} from "@angular/material/tabs";
-import {CoursesCardListComponent} from "../courses-card-list/courses-card-list.component";
-import {MatDialog} from "@angular/material/dialog";
-import {MessagesService} from "../messages/messages.service";
-import {catchError, from, throwError} from "rxjs";
-import {toObservable, toSignal, outputToObservable, outputFromObservable} from "@angular/core/rxjs-interop";
+import {
+  Component,
+  computed,
+  effect,
+  inject,
+  Injector,
+  signal,
+} from '@angular/core';
+import { CoursesService } from '../services/courses.service';
+import { Course, sortCoursesBySeqNo } from '../models/course.model';
+import { MatTab, MatTabGroup } from '@angular/material/tabs';
+import { CoursesCardListComponent } from '../courses-card-list/courses-card-list.component';
+import { MatDialog } from '@angular/material/dialog';
+import { MessagesService } from '../messages/messages.service';
+import { catchError, from, throwError } from 'rxjs';
+import {
+  toObservable,
+  toSignal,
+  outputToObservable,
+  outputFromObservable,
+} from '@angular/core/rxjs-interop';
 
 @Component({
-    selector: 'home',
-    imports: [
-        MatTabGroup,
-        MatTab,
-        CoursesCardListComponent
-    ],
-    templateUrl: './home.component.html',
-    styleUrl: './home.component.scss'
+  selector: 'home',
+  imports: [MatTabGroup, MatTab, CoursesCardListComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss',
 })
 export class HomeComponent {
+  private courseService = inject(CoursesService);
 
+  courses = toSignal(this.courseService.loadAllCourses(), { initialValue: [] });
+
+  beginnerCourses = computed(() => {
+    return this.courses().filter((course) => course.category === 'BEGINNER');
+  });
+
+  intermediateCourses = computed(() => {
+    return this.courses().filter(
+      (course) => course.category === 'INTERMEDIATE'
+    );
+  });
+
+  advancedCourses = computed(() => {
+    return this.courses().filter((course) => course.category === 'ADVANCED');
+  });
 }
--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -1,15 +1,26 @@
-import {Injectable} from "@angular/core";
-import { HttpClient } from "@angular/common/http";
-import {environment} from "../../environments/environment";
-import {firstValueFrom} from "rxjs";
-import {Course} from "../models/course.model";
-import {GetCoursesResponse} from "../models/get-courses.response";
-
+import { inject, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+import { catchError, firstValueFrom, map, Observable, throwError } from 'rxjs';
+import { Course } from '../models/course.model';
+import { GetCoursesResponse } from '../models/get-courses.response';
 
 @Injectable({
-  providedIn: "root"
+  providedIn: 'root',
 })
 export class CoursesService {
+  private url = 'http://localhost:9000/api/';
 
+  private http = inject(HttpClient);
 
+  public loadAllCourses(): Observable<Course[]> {
+    return this.http
+      .get<GetCoursesResponse>(this.url + 'courses', { responseType: 'json' })
+      .pipe(
+        map((response) => response.courses),
+        catchError(() => {
+          return throwError(() => new Error('Error loading courses'));
+        })
+      );
+  }
 }
