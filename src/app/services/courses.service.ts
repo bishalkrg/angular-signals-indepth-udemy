d3df0be90761a5298b09ab891ff38ea66fb54822
Bishal Gautam <bishalkrgautam@gmail.com>
2025-01-17 16:40:41 -0600

    use existing sort function

--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -2,7 +2,7 @@ import { inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment';
 import { catchError, firstValueFrom, map, Observable, throwError } from 'rxjs';
-import { Course } from '../models/course.model';
+import { Course, sortCoursesBySeqNo } from '../models/course.model';
 import { GetCoursesResponse } from '../models/get-courses.response';
 
 @Injectable({
@@ -17,7 +17,10 @@ export class CoursesService {
     return this.http
       .get<GetCoursesResponse>(this.url + 'courses', { responseType: 'json' })
       .pipe(
-        map((response) => response.courses),
+        map((response) => {
+          response.courses.sort(sortCoursesBySeqNo);
+          return response.courses;
+        }),
         catchError(() => {
           return throwError(() => new Error('Error loading courses'));
         })
